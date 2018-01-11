d7df99e3e1a7df4ac92f935c0a7c930bee01f4f2
chenbin <chbin12@126.com>
2018-01-11 17:22:37 +0800

    start

--- coolape/start_mibao.sh
+++ coolape/start_mibao.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-#cd /Users/chenbin/Documents/Working/skynet/
-#cd /Users/niko/Documents/Working/skynet/
-../skynet ./coolape/projects/mibao/config_mibao
+#cd CLServer
+./skynet/skynet ./coolape/projects/mibao/config_mibao
