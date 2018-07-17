cbaea04902f5e74ad45e6bd5ec43c97ec38691da
chenbin <chbin12@126.com>
2018-07-17 18:12:47 +0800

    +

--- coolape/projects/usermgr/service/proManager.lua
+++ coolape/projects/usermgr/service/proManager.lua
@@ -1,3 +1,4 @@
+-- 工程管理后台逻辑处理
 local skynet = require("skynet")
 ---@type CLUtl
 local CLUtl = require("CLUtl")
@@ -10,9 +11,10 @@ local clmanager = nil
 
 local CMD = {}
 
+-- 左边菜单
 function CMD.getLeftMenu(map)
     local ret = {
-        { name = "基本信息", key = "baseinfor" },
+        { name = "基本信息", key = "baseinfor", url="../../projectInfor.html?name=usermgr" },
         { name = "数据库", key = "database", feather = "database" },
         { name = "表设计", key = "tableDesin", feather = "edit" },
         { name = "接口设计", key = "interface", feather = "edit" },
---
+++ coolape/www/js/myutl.js
@@ -0,0 +1,24 @@
+/**
+ * Created by chenbin on 18/7/17.
+ */
+myutl = {}
+
+/*
+ * 跨域调用
+ * url:地址
+ * params：参数
+ * success：成功回调，（result, status, xhr）
+ * error：失败回调，（jqXHR, textStatus, errorThrown）
+ */
+myutl.ajaxJSONP = function (url, params, success, error) {
+    $.ajax({
+            url: url,
+            data: params,
+            dataType: 'jsonp',
+            crossDomain: true,
+            // jsonp:"callback",  //Jquery生成验证参数的名称
+            success: success,  //成功的回调函数,
+            error: error
+        }
+    );
+}
