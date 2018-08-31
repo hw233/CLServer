126d74a69043fa58e9c97cc503be780f0395f694
chenbin <chbin12@126.com>
2018-08-31 23:50:01 +0800

    curve

--- coolape/projects/island/public/cfgUtl.lua
+++ coolape/projects/island/public/cfgUtl.lua
@@ -1,6 +1,33 @@
 local skynet = require "skynet"
+
+---@type curve
+local curve = require("curve")
 ---@class cfgUtl
 cfgUtl = {}
+
+local curves = nil
+
+function cfgUtl.initCurves()
+    curves = {}
+    local curveIns = curve.new(1, 0, 1, curve.easing.linear)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.inQuad)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.outQuad)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.inOutQuad)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.outInQuad)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.inCirc)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.outCirc)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.inOutCirc)
+    table.insert(curves, curveIns)
+    curveIns = curve.new(1, 0, 1, curve.easing.outInCirc)
+    table.insert(curves, curveIns)
+end
 
 -- 取得常量配置
 function cfgUtl.getConstCfg()
@@ -20,6 +47,17 @@ end
 -- 取得建筑
 function cfgUtl.getBuildingByID(id)
     return skynet.call("CLCfg", "lua", "getDataCfg", "DBCFBuildingData", id)
+end
+
+-- 取得成长值
+function cfgUtl.getVal(min, max, curverId, persent)
+    if curves == nil then
+        cfgUtl.initCurves()
+    end
+    ---@type curve
+    local curveins = curves[curverId]
+    local persent = curveins:evaluate(persent)
+    return min + (max - min) * persent
 end
 
 return cfgUtl
